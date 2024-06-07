db641419adc80419ee3da03105d2bd8467df38ca
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-06-07 14:16:46 +0200

    rename endpoint for user

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/airport/', include("flight.urls"), name='flight'),
-    # path('api/airport/', include("user.urls"), name='user'),
+    path('api/user/', include("user.urls"), name='user'),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
